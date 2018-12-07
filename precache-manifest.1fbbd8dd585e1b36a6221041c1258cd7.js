e8b9dec373ba7dbb0958e4062830824a7ff6a0ba
andyka1714 <andyka1714@gmail.com>
2018-12-07 15:21:07 +0800

    Updates

---
+++ precache-manifest.1fbbd8dd585e1b36a6221041c1258cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "119df2174f0d32e57655",
+    "url": "/joke/static/css/main.6f3fa08c.chunk.css"
+  },
+  {
+    "revision": "119df2174f0d32e57655",
+    "url": "/joke/static/js/main.119df217.chunk.js"
+  },
+  {
+    "revision": "2fb841691b8ec10b918f",
+    "url": "/joke/static/js/1.2fb84169.chunk.js"
+  },
+  {
+    "revision": "7e2b52e7168a8d5b46ff",
+    "url": "/joke/static/js/runtime~main.7e2b52e7.js"
+  },
+  {
+    "revision": "e09a3ce5e6505d42057017e3c634d279",
+    "url": "/joke/index.html"
+  }
+];
